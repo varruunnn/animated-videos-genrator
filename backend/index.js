f5dca7694658148a5e9e72df0b0125212104a94f
varruunnn <varun.choudhary2512@gmail.com>
2025-05-18 12:27:06 +0530

    health checkup

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/generate-animation', require('./routes/animation'));
 app.use('/api/profile', require('./routes/profile'));
+app.use('/api/health', require('./routes/health'));
 
 
 const PORT = process.env.PORT || 3001;
